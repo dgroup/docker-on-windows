446cc0c6ef850e18c7d6706951dee59b65c0e9dd
dubinka <dubinka@netcracker.com>
2017-11-08 13:26:48 +0530

    Script cleaning

--- docker.release-image.sh
+++ docker.release-image.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-docker_repository="<repository_name>"
+docker_remote_repository="<repository_name>"
 docker_image_name="<image_name>"
-docker_tags=(1.0 latest 3.4.1-jdk8-alpine)
+docker_tags=(1.0 1.1.1 latest)
 
 ./docker.build-image.sh ${docker_image_name} $(pwd) Dockerfile
 
-docker login ${docker_repository}
+docker login ${docker_remote_repository}
 
 for tagName in ${docker_tags[*]}
 do
-    full_image_name="${docker_repository}/${docker_image_name}:${tagName}"
+    full_image_name="${docker_remote_repository}/${docker_image_name}:${tagName}"
     docker tag ${docker_image_name} ${full_image_name}
     docker push ${full_image_name}
 done
